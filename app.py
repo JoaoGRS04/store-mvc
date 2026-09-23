8ccdbefabb84d9b0d18ccb972b3afcb1ca95ffd8
JoaoGRS04 <joaoshowgabriel@gmail.com>
2026-09-22 22:22:17 -0300

    feat: initialize main function

--- app.py
+++ app.py
@@ -1,2 +1,4 @@
-# TODO: Fazer tudo funcionar! 
-# Acho que na segunda posso terminar depois da 1:1 com o Rodrigo. Vou pedir um aumento.
+def main():
+    pass
+if __name__ == '__main__':
+    main()
